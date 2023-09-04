3c9d7589162e4627d63c569f2a5c57a4dee77233
Marco Falcão <maffalcao@gmail.com>
2023-09-04 18:16:39 -0300

    creation of shopping components

---
+++ app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent {
+  ingredients: Ingredient[]
+
+  constructor() {
+    this.ingredients = []
+  }
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient)
+  }
+
+}
