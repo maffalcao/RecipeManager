80c45a345cfbd8ea85679ce59ff2c932632d8e96
marco <maffalcao@gmail.com>
2024-03-25 12:38:23 -0300

    dinamicy components

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,20 +1,25 @@
 import { NgForm } from '@angular/forms';
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild, ViewContainerRef } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode = true
-  isLoading = false
-  error:string = null
+  isLoading = false  
+  private closeSub: Subscription;
+  
+  @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
 
   constructor(private authService: AuthService, private rounter: Router) {}
-  
+ 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode
   }
@@ -33,10 +38,9 @@ export class AuthComponent {
               this.isLoading = false
               this.rounter.navigate([''])
             },
-            error: (errorMessage) => {
-              console.log(errorMessage)
-              this.error = errorMessage             
+            error: (errorMessage) => {              
               this.isLoading = false
+              this.showErrorAlert(errorMessage)
             }            
         }) 
     }
@@ -51,12 +55,32 @@ export class AuthComponent {
               this.isLoading = false
               this.rounter.navigate([''])
             },
-            error: (errorMessage) => {
-              console.log(errorMessage)
-              this.error = errorMessage                            
+            error: (errorMessage) => {              
               this.isLoading = false
+              this.showErrorAlert(errorMessage)
             }            
         })
     }        
-  }  
+  }
+
+  // onHangleError() {
+  //   this.error = null;
+  // }
+
+  private showErrorAlert(errorMessage: string) {     
+    const componentRef = this.alertHost.viewContainerRef.createComponent(AlertComponent);
+
+    componentRef.instance.message = errorMessage;
+
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      
+        this.closeSub.unsubscribe();
+        this.alertHost.viewContainerRef.clear()
+        
+    })    
+  }
+
+  ngOnDestroy(): void {
+    this.closeSub.unsubscribe();
+  }
 }
